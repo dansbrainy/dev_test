bbcefce0b7efddd8a49d960a88adadbb947696c5
Daniel Kamanda <danskamanda@gmail.com>
2021-08-21 08:54:43 +0200

    try navs = hard coded-first

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
 import React, { useEffect } from "react";
-import MessageBox from "../components/MessageBox";
-import LoadingBox from "../components/LoadingBox";
+import { Link, NavLink } from "react-router-dom";
+// import MessageBox from "../components/MessageBox";
+// import LoadingBox from "../components/LoadingBox";
 
 export default function HomeScreen() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row center">
+        <nav>
+          <NavLink className="nav-item" to="">sports</NavLink>
+          <NavLink className="nav-item" to="">live & real</NavLink>
+          <NavLink className="nav-item" to="">casino</NavLink>
+          <NavLink className="nav-item" to="">esports</NavLink>
+          <NavLink className="nav-item" to="">vegas</NavLink>
+        </nav>
+        
+      </div>
+    </div>
+  );
 }
