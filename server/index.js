10c19549d7b3db8be74b0c46a4dd4cc1f0c69a50
Daniel Kamanda <danskamanda@gmail.com>
2021-08-22 10:10:12 +0200

    update index.js for heroku

--- server/index.js
+++ server/index.js
@@ -23,6 +23,13 @@ app.get("/api/bets", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
 // console.log that your server is up and running
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
