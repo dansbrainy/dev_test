2cc972e46c4f6232520857b37f8af3785105ea8d
Daniel Kamanda <danskamanda@gmail.com>
2021-08-22 23:19:34 +0200

    slight modification on styles

--- client/src/components/LoadingBox.js
+++ client/src/components/LoadingBox.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function LoadingBox() {
   return (
-    <div>
+    <div className="row center">
       <i className="fa fa-spinner fa-spin"></i> Loading...
     </div>
-  )
+  );
 }
